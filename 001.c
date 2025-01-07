ff5ba296a5c267125afa8e7f2a40d052585856d4
Ayush Yadav <134876624+ayush-iit-tp@users.noreply.github.com>
2025-01-07 23:08:58 +0530

    Add files via upload

---
+++ 001.c
@@ -0,0 +1,9 @@
+#include <stdio.h> // pre-processor-directive helps in including functions in pre processing stage by the compiler
+//we can use more ppd also
+int main() // int tells return value of this main function is an integer
+{
+     printf("Hello World"); // C program will execute firstly this line
+return 0;  // stop this print function and return 0
+}
+
+// This is boilerplate code: means the template except this printf line
